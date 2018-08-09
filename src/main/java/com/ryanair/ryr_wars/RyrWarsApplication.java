2d098953fa89cc874c48612fe794f65d4cdd57b4
Cuesta Castro <cuestad@ryanair.com>
2018-08-09 17:04:46 +0200

    Initialize

---
+++ src/main/java/com/ryanair/ryr_wars/RyrWarsApplication.java
@@ -0,0 +1,12 @@
+package com.ryanair.ryr_wars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyrWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RyrWarsApplication.class, args);
+	}
+}
